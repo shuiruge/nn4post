882276f7022b4d1155f840bd8f0e326a5b0544ea
pen. ciun. jian <shuiruge@hotmail.com>
2017-07-28 15:06:12 +0800

    Wrote over a rough version. Tested on `DIM = 1` and `NUM_PEAKS=1`, which is the simplest test. So far so good.

--- sample/nn4post_np.py
+++ sample/nn4post_np.py
@@ -28,6 +28,10 @@ class FGMD(object):
             Gaussian distribution.
         
         For other notations employed herein, c.f. `'../docs/nn4post.tm'`.
+        
+    NOTE:
+        `a` is initialized by `np.ones`; `b` and `w` are both initialized
+        randomly by standard Gaussian.
                    
     Args:
         dim: int
@@ -46,7 +50,7 @@ class FGMD(object):
         self._dim = dim
         self._num_peaks = num_peaks
         
-        self._a = np.random.normal(size=[self._num_peaks])
+        self._a = np.ones(shape=[self._num_peaks])
         self._b = np.random.normal(size=[self._dim, self._num_peaks])
         self._w = np.random.normal(size=[self._dim, self._num_peaks])
         
@@ -78,7 +82,7 @@ class FGMD(object):
         """
         
         log_cat = np.expand_dims(
-            np.log(np.square(self._a)),
+            np.log(self._cat),
             axis=0)  # shape: [1, self._num_peaks]
         log_gaussian = np.sum(
             (   -0.5 * np.square(np.expand_dims(theta, axis=2) \
@@ -160,29 +164,58 @@ class FGMD(object):
         self._w = value
         return None
     
+    def copy(self):
+        fgmd = FGMD(self.get_dim(), self.get_num_peaks())
+        fgmd.set_a(self.get_a())
+        fgmd.set_b(self.get_b())
+        fgmd.set_w(self.get_w())
+        return fgmd
+    
 
 def performance(log_p, fgmd, num_samples=100):
     """ Use KL-divergence as performance of fitting a posterior `log_p` by a
-        finite Gaussian mixture distribution (FGMD).\
+        finite Gaussian mixture distribution (FGMD).
+        
+    NOTE:
+        Even though KL-divergence is principly non-negative, the `log_p` and
+        `fgmd.log_pdf` may not be normalized, so that the KL-divergence is
+        "non-negative up to an overall constant".
     
     Args:
         log_p: np.array(shape=[fgmd.get_dim()]) -> float
         fgmd: FGMD
         num_sample: int
             For Monte Carlo integration.
+            
     Returns:
         float
     """
     
     thetae = fgmd.sample(num_samples)
-    log_q = fgmd.log_pdf
-    
-    kl_divergence = np.mean(log_p(thetae) - log_q(thetae))
+
+    kl_divergence = np.mean(fgmd.log_pdf(thetae) - log_p(thetae))
     return kl_divergence
 
 
 
 def nabla_perfm(log_p, fgmd, num_samples=100):
+    """ :math:`\nabla_z \textrm(KL) (q(z) || p)`, where :math:`z := (a, b, w)`,
+        :math:`q` is the PDF of `fgmd` (whose logrithm is `fgmd.log_pdf`), and
+        `log_p` is the logrithm of PDF of :math:`p`.
+    
+    Args:
+        log_p: np.array(shape=[fgmd.get_dim()]) -> float
+        fgmd: FGMD
+        num_samples: int
+            Number of samples in the Monte Carlo integration herein.
+            
+    Returns:
+        [
+            nabla_perfm_by_a: np.array(shape=fgmd.get_a().shape),
+            nabla_perfm_by_b: np.array(shape=fgmd.get_b().shape),
+            nabla_perfm_by_w: np.array(shape=fgmd.get_w().shape),
+        ]
+    """
     
     num_peaks = fgmd.get_num_peaks()
     dim = fgmd.get_dim()
@@ -203,10 +236,10 @@ def nabla_perfm(log_p, fgmd, num_samples=100):
             nabla_beta_i: np.array(shape=?)
                 :math:`\frac{\partial \beta}{\partial z}`
         Returns:
-            np.array(shape=)
+            np.array(shape=?)
         """
         
-        x = np.expand_dims(log_p(thetae) - log_q(thetae) - 1, axis=1) \
+        x = np.expand_dims(log_q(thetae) - log_p(thetae) - 1, axis=1) \
             * proportion  # shape: [num_samples, num_peaks]
             
         if len(nabla_beta.shape) == 1:  # like `a`, with shape: [num_peaks].
@@ -228,18 +261,71 @@ def nabla_perfm(log_p, fgmd, num_samples=100):
                        + np.expand_dims(b, axis=0)) * np.expand_dims(thetae, axis=2) \
                       + 1 / np.expand_dims(w, axis=0)  # shape: [num_samples, dim, num_peaks]
 
-    return (
-        nabla_perfm_sub(nabla_beta_by_a),
-        nabla_perfm_sub(nabla_beta_by_b),
-        nabla_perfm_sub(nabla_beta_by_w)
-        )
+    return [
+        nabla_perfm_sub(nabla_beta_by_a),  # shape: [num_peaks]
+        nabla_perfm_sub(nabla_beta_by_b),  # shape: [dim, num_peaks]
+        nabla_perfm_sub(nabla_beta_by_w),  # shape: [dim, num_peaks]
+        ]
 
+
+def gradient_descent(log_p, fgmd, num_steps, learning_rate,
+        num_samples=100, clip_limit=10**2):
+    """ Using gradient descent method to update `fgmd` by **minimizing** the
+        KL-divergence (as the performance) between `log_p` and `fgmd.log_pdf`.
+        
+    NOTE:
+        Sometimes the gradients shall be clipped, since there's `1/a`, `1/w`
+        in the gradients, which may lead to an overflow (thus returns `nan`)
+        when `a` or `w` becomes small enough.
+        
+    Args:
+        log_p: np.array(shape=[fgmd.get_dim()]) -> float
+        fgmd: FGMD
+        num_steps: int
+            Number of times of updating by gradient descent.
+        learning_rate: float
+            `z += -learning_rate * gradient_by_z for z in [a, b, w]`.
+        num_samples: int
+            Number of samples in the Monte Carlo integration herein.
+        clip_limit: float or None
+            Clipping each element in the gradients of `a`, `b`, and `w` by
+            `clip_limit`. If `None`, then no clipping to be established.
+    
+    Returns:
+        FGMD
+            This instance of `FGMD` is **not** the one in the argument (i.e.
+            the `fgmd`), but a new instance initially copied from the `fgmd`.
+    """
+    
+    fgmd0 = fgmd.copy()
+    
+    for step in range(num_steps):
+        
+        gradients = nabla_perfm(log_p, fgmd0, num_samples)
+        
+        # Clip the gradients
+        for i, grad in enumerate(gradients):
+            gradients[i] = np.clip(grad, -clip_limit, clip_limit)
+        
+        delta_a = -learning_rate * gradients[0]
+        delta_b = -learning_rate * gradients[1]
+        delta_w = -learning_rate * gradients[2]
+        
+        a = fgmd0.get_a()
+        b = fgmd0.get_b()
+        w = fgmd0.get_w()
+        
+        fgmd0.set_a(a + delta_a)
+        fgmd0.set_b(b + delta_b)
+        fgmd0.set_w(w + delta_w)
+       
+    return fgmd0
 
 
 # --- Test ---
 
-DIM = 100
-NUM_PEAKS = 2
+DIM = 1
+NUM_PEAKS = 1
 
 NUM_SAMPLES = 50
 
@@ -249,3 +335,10 @@ log_p = lambda theta: -0.5 * np.sum(np.square(theta), axis=1)
 
 for _ in nabla_perfm(log_p, fgmd, num_samples=NUM_SAMPLES):
     print(_.shape)
+
+print(performance(log_p, fgmd))
+fgmd = gradient_descent(log_p, fgmd, 500, 0.001)
+print(performance(log_p, fgmd))
+print(fgmd.get_a())
+print(fgmd.get_b())  # shall be near `0`.
+print(fgmd.get_w())  # shall be near `1` or `-1`.
