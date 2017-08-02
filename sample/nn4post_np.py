d7d3be1a0c717afe145620e64020f57acfa08b5d
pen. ciun. jian <shuiruge@hotmail.com>
2017-08-02 12:26:17 +0800

    Finized. Made some experiments, finding that `_DEFAULT_CLIP_LIMIT` had better to be `np.inf`, i.e. no gradient-clipping.

--- sample/nn4post_np.py
+++ sample/nn4post_np.py
@@ -7,6 +7,12 @@ Numpy Version.
 import numpy as np
 
 
+_DEFAULT_NUM_SAMPLES = 10 ** 2
+_DEFAULT_EPSILON = np.exp(-10)
+#_DEFAULT_CLIP_LIMIT = np.exp(7)
+_DEFAULT_CLIP_LIMIT = np.inf  # test!
+
+
 def no_underflow(epsilon, x):
     """ If x underflow, then return `epsilon`, as protection in `1/x`.
         Args:
@@ -17,8 +23,8 @@ def no_underflow(epsilon, x):
     return np.where(x==0, epsilon, x)
 
 
-class FGMD(object):
-    """ Finite Gaussian Mixture Distribution.
+class CGMD(object):
+    """ Categorical Gaussian Mixture Distribution.
     
     C.f. WikiPedia_.
     
@@ -54,7 +60,7 @@ class FGMD(object):
         get_cat, get_components
     """
     
-    def __init__(self, dim, num_peaks, epsilon=np.exp(-10)):
+    def __init__(self, dim, num_peaks, epsilon=_DEFAULT_EPSILON):
         
         self._dim = dim
         self._num_peaks = num_peaks
@@ -139,7 +145,7 @@ class FGMD(object):
    
 
     def sample(self, num_samples):
-        """ Randomly sample `num_samples` samples from FGMD.
+        """ Randomly sample `num_samples` samples from CGMD.
         
         Args:
             num_samples: int
@@ -155,7 +161,7 @@ class FGMD(object):
         components = self.get_components()
         
         def generate_sample():
-            """ Generate one sample from FGMD. """
+            """ Generate one sample from CGMD. """
             
             index = np.random.choice(num_peaks, p=cat)
             samp = np.random.normal(*components[index])
@@ -240,27 +246,29 @@ class FGMD(object):
         return None
     
     def copy(self):
-        fgmd = FGMD(self.get_dim(),
+        cgmd = CGMD(self.get_dim(),
                     self.get_num_peaks(),
                     self.get_epsilon())
-        fgmd.set_a(self.get_a())
-        fgmd.set_b(self.get_b())
-        fgmd.set_w(self.get_w())
-        return fgmd
+        cgmd.set_a(self.get_a())
+        cgmd.set_b(self.get_b())
+        cgmd.set_w(self.get_w())
+        return fgmc
 
 
-def performance(log_p, fgmd, num_samples=100):
+def performance(log_p, cgmd, num_samples=_DEFAULT_NUM_SAMPLES):
     """ Use KL-divergence as performance of fitting a posterior `log_p` by a
-        finite Gaussian mixture distribution (FGMD).
+        finite Gaussian mixture distribution (CGMD).
         
     NOTE:
         Even though KL-divergence is principly non-negative, the `log_p` and
-        `fgmd.log_pdf` may not be normalized, so that the KL-divergence is
+        `cgmd.log_pdf` may not be normalized, so that the KL-divergence is
         "non-negative up to an overall constant".
     
     Args:
-        log_p: np.array(shape=[fgmd.get_dim()], dtype=float32) -> float
-        fgmd: FGMD
+        log_p: Map(**{thetae: np.array(shape=[None, cgmd.get_dim()],
+                                       dtype=float32)},
+                   float)
+        cgmd: CGMD
         num_sample: int
             For Monte Carlo integration.
             
@@ -268,15 +276,15 @@ def performance(log_p, fgmd, num_samples=100):
         float
     """
     
-    thetae = fgmd.sample(num_samples)
+    thetae = cgmd.sample(num_samples)
 
-    kl_divergence = np.mean(fgmd.log_pdf(thetae) - log_p(thetae))
+    kl_divergence = np.mean(cgmd.log_pdf(thetae) - log_p(thetae))
     return kl_divergence
 
 
-def nabla_perfm(fgmd, log_p, epsilon, clip_limit, num_samples):
-    """ :math:`\nabla_z \textrm(KL) (q(z) || p)`, where :math:`z := (a, b, w)`,
-        :math:`q` is the PDF of `fgmd` (whose logrithm is `fgmd.log_pdf`), and
+def nabla_perfm(log_p, cgmd, epsilon, clip_limit, num_samples):
+    """ :math:`\nabla_z \textrm(KL) (q(z) || p_D)`, where :math:`z := (a, b, w)`,
+        :math:`q` is the PDF of `cgmd` (whose logrithm is `cgmd.log_pdf`), and
         `log_p` is the logrithm of PDF of :math:`p`.
         
         Helper function for `gradient_descent()`.
@@ -286,14 +294,18 @@ def nabla_perfm(fgmd, log_p, epsilon, clip_limit, num_samples):
         in the gradients, which may lead to an overflow (thus returns `nan`)
         when `a` or `w` becomes small enough.
         
-        And the epsilon is added (BUGS HEREIN) to the denominator of `1/a` and
-        '1/w` in the computation of gradients to avoid `nan`.
+        And the epsilon is added to the denominator of `1/a` andb '1/w` in the
+        computation of gradients to avoid `nan`.
         
     Args:
-        fgmd: FGMD
-        log_p: np.array(shape=[fgmd.get_dim()], dtype=float32) -> float
+        log_p: Map(**{thetae: np.array(shape=[None, cgmd.get_dim()],
+                                       dtype=float32)},
+                   float)
+            Vectorized, as `None` hints.
+        cgmd: CGMD
         epsilon: float
-            asdf   BUGS HEREIN
+            Positive. Added to the denominator of `1/a` andb '1/w` in the
+            computation of gradients to avoid `nan`.
         clip_limit: float or None
             Clipping each element in the gradients of `a`, `b`, and `w` by
             `clip_limit`. If `None`, then no clipping to be established.
@@ -302,23 +314,23 @@ def nabla_perfm(fgmd, log_p, epsilon, clip_limit, num_samples):
             
     Returns:
         [
-            nabla_perfm_by_a: np.array(shape=fgmd.get_a().shape,
+            nabla_perfm_by_a: np.array(shape=cgmd.get_a().shape,
                                        dtype=float32),
-            nabla_perfm_by_b: np.array(shape=fgmd.get_b().shape,
+            nabla_perfm_by_b: np.array(shape=cgmd.get_b().shape,
                                        dtype=float32),
-            nabla_perfm_by_w: np.array(shape=fgmd.get_w().shape,
+            nabla_perfm_by_w: np.array(shape=cgmd.get_w().shape,
                                        dtype=float32),
         ]
     """
 
-    log_q = fgmd.log_pdf
-    a = fgmd.get_a()
-    b = fgmd.get_b()
-    w = fgmd.get_w()
+    log_q = cgmd.log_pdf
+    a = cgmd.get_a()
+    b = cgmd.get_b()
+    w = cgmd.get_w()
 
-    thetae = fgmd.sample(num_samples)
+    thetae = cgmd.sample(num_samples)
 
-    beta_max, delta_beta = fgmd.beta(thetae)
+    beta_max, delta_beta = cgmd.beta(thetae)
     # shape: [num_samples, num_peaks]
     proportion = np.exp(delta_beta) \
                / np.expand_dims(np.sum(np.exp(delta_beta), axis=1), axis=1)
@@ -378,10 +390,16 @@ def nabla_perfm(fgmd, log_p, epsilon, clip_limit, num_samples):
     return gradients
 
 
-def gradient_descent(log_p, fgmd, num_steps, learning_rate,
-        epsilon=np.exp(-10), clip_limit=np.exp(7), num_samples=100):
-    """ Update `fgmd` by using gradient descent method which **minimizes** the
-        KL-divergence (as the performance) between `log_p` and `fgmd.log_pdf`.
+def gradient_descent(
+        log_p,
+        cgmd,
+        learning_rate,
+        epsilon=_DEFAULT_EPSILON,
+        clip_limit=_DEFAULT_CLIP_LIMIT,
+        num_samples=_DEFAULT_NUM_SAMPLES):
+    """ Update `cgmd` **once** by using gradient descent method which
+        **minimizes** the KL-divergence (as the performance) between `log_p`
+        and `cgmd.log_pdf`.
         
     Numerical Treatments:
         Sometimes the gradients shall be clipped, since there's `1/a`, `1/w`
@@ -392,10 +410,11 @@ def gradient_descent(log_p, fgmd, num_steps, learning_rate,
         '1/w` in the computation of gradients to avoid `nan`.
         
     Args:
-        fgmd: FGMD
-        log_p: np.array(shape=[fgmd.get_dim()], dtype=float32) -> float
-        num_steps: int
-            Number of times of updating by gradient descent.
+        log_p: Map(**{thetae: np.array(shape=[None, cgmd.get_dim()],
+                                       dtype=float32)},
+                   float)
+            Vectorized, as `None` hints.
+        cgmd: CGMD
         learning_rate: float
             `z += -learning_rate * gradient_by_z for z in [a, b, w]`.
         num_samples: int
@@ -405,37 +424,91 @@ def gradient_descent(log_p, fgmd, num_steps, learning_rate,
             `clip_limit`. If `None`, then no clipping to be established.
     
     Returns:
-        FGMD
-            The updated `fgmd`.
+        CGMD
+            The updated `cgmd`.
     """
+
+    gradients = nabla_perfm(log_p,
+                            cgmd,
+                            num_samples=num_samples,
+                            epsilon=epsilon,
+                            clip_limit=clip_limit)
+    
+    delta_a = -learning_rate * gradients[0]
+    delta_b = -learning_rate * gradients[1]
+    delta_w = -learning_rate * gradients[2]
+    
+    a = cgmd.get_a()
+    b = cgmd.get_b()
+    w = cgmd.get_w()
+    
+    cgmd.set_a(a + delta_a)
+    cgmd.set_b(b + delta_b)
+    cgmd.set_w(w + delta_w)
+
+    return cgmd
+
+
+def sgd(log_posterior,
+        data_batchs,
+        cgmd,
+        learning_rate,
+        epsilon=_DEFAULT_EPSILON,
+        clip_limit=_DEFAULT_CLIP_LIMIT,
+        num_samples=_DEFAULT_NUM_SAMPLES):
+    """ Update `cgmd` by using stochastic gradient descent (SGD) method which
+        **minimizes** the KL-divergence (as the performance) between `log_p`
+        and `cgmd.log_pdf`.
         
-    for step in range(num_steps):
+    Numerical Treatments:
+        Sometimes the gradients shall be clipped, since there's `1/a`, `1/w`
+        in the gradients, which may lead to an overflow (thus returns `nan`)
+        when `a` or `w` becomes small enough.
         
-        try:
-            gradients = nabla_perfm(fgmd, log_p,
-                                    num_samples=num_samples,
-                                    epsilon=epsilon,
-                                    clip_limit=clip_limit)
-            
-            delta_a = -learning_rate * gradients[0]
-            delta_b = -learning_rate * gradients[1]
-            delta_w = -learning_rate * gradients[2]
-            
-            a = fgmd.get_a()
-            b = fgmd.get_b()
-            w = fgmd.get_w()
-            
-            fgmd.set_a(a + delta_a)
-            fgmd.set_b(b + delta_b)
-            fgmd.set_w(w + delta_w)
+        And the epsilon is added (BUGS HEREIN) to the denominator of `1/a` and
+        '1/w` in the computation of gradients to avoid `nan`.
         
-        except Exception as e:
-            
-            print('ERROR')
-            print('step at {0}'.format(step))
-            raise Exception(e)
-           
-    return fgmd
+    Args:
+        log_posterior:
+            Map(**{data: (x: np.array(shape=None, dtype=None),
+                          y: np.array(shape=None, dtype=None))
+                   theta: np.array(shape=[cgmd.get_dim()], dtype=float32)},
+                float)
+            where `np.array(shape=None, dtype=None)` is an element in
+            `data_batchs`.
+        data_batchs: [np.array(shape=None, dtype=None)]
+        cgmd: CGMD
+        learning_rate: float
+            `z += -learning_rate * gradient_by_z for z in [a, b, w]`.
+        num_samples: int
+            Number of samples in the Monte Carlo integration herein.
+        clip_limit: float or None
+            Clipping each element in the gradients of `a`, `b`, and `w` by
+            `clip_limit`. If `None`, then no clipping to be established.
+    
+    Returns:
+        CGMD
+            The updated `cgmd`.
+    """
+    
+    for data in data_batchs:
+        
+        # Recall that `gradient_descent()` requires its argument `log_p` being
+        # vectorized
+        def log_p(thetae):
+            num_thetae = thetae.shape[0]
+            return np.array([log_posterior(data, thetae[i,:])
+                             for i in range(num_thetae)])
+        
+        gradient_descent(
+            log_p,
+            cgmd,
+            learning_rate,
+            epsilon=epsilon,
+            clip_limit=clip_limit,
+            num_samples=num_samples)
+
+    return cgmd
 
 
 if __name__ == '__main__':
@@ -445,38 +518,49 @@ if __name__ == '__main__':
     
     import tools
 
-    DIM = 100
-    NUM_PEAKS = 2
+    DIM = 1
+    NUM_PEAKS = 10
     
-    fgmd = FGMD(DIM, NUM_PEAKS)
+    cgmd = CGMD(DIM, NUM_PEAKS)
     log_p = lambda theta: (-0.5 * np.sum(np.square(theta), axis=1)
                            - 0.5 * np.log(2 * np.pi))
     
     # --- Before gradient descent
-    #print('b: {0}'.format(fgmd.get_b()))
-    #print('w: {0}'.format(fgmd.get_w()))
-    #print('cat: {0}'.format(fgmd.get_cat()))
-    #print('components: {0}'.format(fgmd.get_components()))
-    old_performance = performance(log_p, fgmd)
+    #print('b: {0}'.format(cgmd.get_b()))
+    #print('w: {0}'.format(cgmd.get_w()))
+    #print('cat: {0}'.format(cgmd.get_cat()))
+    #print('components: {0}'.format(cgmd.get_components()))
+    old_performance = performance(log_p, cgmd)
     print('performance: {0}'.format(old_performance))
     
     # --- Making gradient descent
     with tools.Timer():
-        gradient_descent(log_p, fgmd, num_steps=1000, learning_rate=0.001)
+        for step in range(1000):
+            gradient_descent(log_p, cgmd, learning_rate=0.0001)
         
     # After gradient descent
     print('After updating ......\n')
-    #print('updated a: {0}'.format(fgmd.get_a()))
-    #print('updated b (shall be near `0`): {0}'.format(fgmd.get_b()))
-    #print('updated w (shall be near `+-1`): {0}'.format(fgmd.get_w()))
-    #print('updated cat: {0}'.format(fgmd.get_cat()))
-    #print('updated components: {0}'.format(fgmd.get_components()))
+    #print('updated a: {0}'.format(cgmd.get_a()))
+    #print('updated b (shall be near `0`): {0}'.format(cgmd.get_b()))
+    #print('updated w (shall be near `+-1`): {0}'.format(cgmd.get_w()))
+    #print('updated cat: {0}'.format(cgmd.get_cat()))
+    #print('updated components: {0}'.format(cgmd.get_components()))
     
     # --- Improvement by gradient descent
-    new_performance = performance(log_p, fgmd)
+    new_performance = performance(log_p, cgmd)
     print('\nupdated performance:\n\t{0}  -->  {1}\n'.format(
             old_performance, new_performance))
-
+    
+    # --- Plot the result out
+    #     **Valid only when `DIM = 1`**
+    assert DIM == 1
+    import matplotlib.pyplot as plt
+    boundary = 2
+    num_x = boundary * 10
+    x = np.linspace(-boundary, boundary, num_x)
+    plt.plot(x, log_p(np.array([[_] for _ in x])))
+    plt.plot(x, cgmd.log_pdf(np.array([[_] for _ in x])), '--')    
+    plt.show()
 
 #    # --- The Second Test ---
 #
@@ -484,11 +568,11 @@ if __name__ == '__main__':
 #    import matplotlib.pyplot as plt
 #
 #    DIM = 1
-#    NUM_PEAKS = 1
+#    NUM_PEAKS = 100
 #    
-#    fgmd = FGMD(DIM, NUM_PEAKS)
+#    cgmd = CGMD(DIM, NUM_PEAKS)
 #    
-#    NUM_DATA = 100
+#    NUM_DATA = 1000
 #    THETA_STAR = 1
 #    x = np.linspace(-1, 1, NUM_DATA)
 #    mu = - np.sqrt(np.sum(np.square(x))) * THETA_STAR
@@ -496,12 +580,13 @@ if __name__ == '__main__':
 #    print(mu, one_by_sigma)
 #
 #    def log_p(theta):
+#        noise = np.random.normal()
 #        return -0.5 * (np.sum(np.square(one_by_sigma * (theta - mu)), axis=1)
 #                       + np.log(2 * np.pi)
-#                       - np.log(np.square(one_by_sigma)))
+#                       - np.log(np.square(one_by_sigma))) + noise
 #    
 #    # --- Before gradient descent
-#    old_performance = performance(log_p, fgmd)
+#    old_performance = performance(log_p, cgmd)
 #    print('performance: {0}'.format(old_performance))
 #    
 #    # --- Making gradient descent
@@ -511,12 +596,12 @@ if __name__ == '__main__':
 #        performance_log = []
 #        performance_log.append(np.log(old_performance))
 #        
-#        for epoch in range(epochs): 
-#            gradient_descent(log_p, fgmd, num_steps=10, learning_rate=0.001, num_samples=10**3)
-#            new_performance = performance(log_p, fgmd)
+#        for epoch in range(epochs):
+#            gradient_descent(log_p, cgmd, learning_rate=0.001, num_samples=10**2)
+#            new_performance = performance(log_p, cgmd)
 #            performance_log.append(np.log(new_performance))
 #            
-#    new_performance = performance(log_p, fgmd)
+#    new_performance = performance(log_p, cgmd)
 #    print('\nupdated performance:\n\t{0}  -->  {1}\n'.format(
 #            old_performance, new_performance))
 #    
