fcd42907d0c268da26a7eea6676cfc85d70fec3e
pen. ciun. jian <shuiruge@hotmail.com>
2017-08-02 12:28:28 +0800

    Made more experiments.

--- sample/bayesian_inference.py
+++ sample/bayesian_inference.py
@@ -9,7 +9,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def log_post(data, theta, f, prior_is_unknown=True):
+def log_posterior(data, theta, f, prior_is_unknown=True):
     """ Logrithmic posterior, up to :math:`ln p(D)`.
     
     Notations:
@@ -20,8 +20,8 @@ def log_post(data, theta, f, prior_is_unknown=True):
                y: np.array(shape=[num_data, y_dim]))
             with length of list `num_data`.
         theta: np.array(shape=[theta_dim], dtype=float32)
-        f: Map((theta: np.array(shape=[theta_dim], dtype=float32),
-                x: np.array(shape=[x_dim])),
+        f: Map(**{theta: np.array(shape=[theta_dim], dtype=float32),
+                  x: np.array(shape=[x_dim])},
                np.array(shape=[y_dim]))
     
     Returns:
@@ -31,10 +31,9 @@ def log_post(data, theta, f, prior_is_unknown=True):
     x, y = data  # shape: [num_data, x_dim] and [num_data, y_dim]
     num_data, x_dim = x.shape
     num_data, y_dim = y.shape
-    theta_dim = theta.shape
 
     y_sigma=np.ones(shape=[num_data, y_dim]) * 1
-    theta_sigma = np.ones(shape=theta_dim) * 1000
+    theta_sigma = np.ones(shape=theta.shape) * 1
     
     f_array = np.array([f(theta, x[i,:]) for i in range(num_data)])
     
@@ -62,7 +61,9 @@ if __name__ == '__main__':
     y_dim = 1
     theta_dim = x_dim
 
-    num_data = 100
+    num_data = 1000
+    num_batchs = 50
+    assert num_data % num_batchs == 0
     
     def f(theta, x):
         return np.array([np.sum(theta * x)])
@@ -70,59 +71,74 @@ if __name__ == '__main__':
     x = np.array([np.linspace(-1, 1, num_data) for i in range(x_dim)])
     x = x.T  # shape: [num_data, x_dim]
     target_theta = np.ones(shape=[x_dim])
-    @np.vectorize
     def target_f(x):
         return np.sum(target_theta * x)
     y = target_f(x) \
       + np.random.normal(loc=0.0, scale=0.0, size=[num_data, y_dim])
-    data = (x, y)
-    print(log_post(data, target_theta, f))
+    np.random.RandomState(seed=12345).shuffle(x)
+    np.random.RandomState(seed=12345).shuffle(y)
+    x_batchs = np.split(x, num_batchs)
+    y_batchs = np.split(y, num_batchs)
+    data_batchs = list(zip(x_batchs, y_batchs))
+    print(log_posterior(data_batchs[0], target_theta, f))
 
     
     
     # --- Test nn4post ---
     
-    from nn4post_np import FGMD, performance, gradient_descent
+    from nn4post_np import CGMD, performance, sgd
     
     DIM = theta_dim
-    NUM_PEAKS = 1
+    NUM_PEAKS = 10
     
-    fgmd = FGMD(DIM, NUM_PEAKS)
-    def log_p(theta):
-        return log_post(data, theta, f)
+    cgmd = CGMD(DIM, NUM_PEAKS)
+    def log_p(data, theta):
+        return log_posterior(data, theta, f)
     
     # --- Before gradient descent
-    old_performance = performance(log_p, fgmd)
+    data = data_batchs[0]
+    old_performance = performance(lambda theta: log_p(data, theta), cgmd)
     print('performance: {0}'.format(old_performance))
     
     # --- Making gradient descent
     import tools
     with tools.Timer():
         
-        num_epochs = 1000
+        num_epochs = 30
         performance_log = []
         performance_log.append(np.log(old_performance))
         
         for epoch in range(num_epochs):
             
-            gradient_descent(log_p, fgmd, num_steps=10, learning_rate=0.0001)
-            new_performance = performance(log_p, fgmd)
+            sgd(log_p, data_batchs, cgmd, learning_rate=0.001, num_samples=10**2)
+            new_performance = performance(lambda theta: log_p(data, theta), cgmd)
             performance_log.append(np.log(new_performance))
     
     # --- Improvement by gradient descent
-    new_performance = performance(log_p, fgmd)
+    new_performance = performance(lambda theta: log_p(data, theta), cgmd)
     print('\nupdated performance:\n\t{0}  -->  {1}\n'.format(
             old_performance, new_performance))
     
     plt.plot(performance_log)
-    plt.show() 
+    plt.show()
     
-    # --- `theta_hat` ---
+    # --- Plot the result out
+    #     **Valid only when `DIM = 1`**
+    assert DIM == 1
+    boundary = 5
+    num_x = boundary * 10
+    x = np.linspace(-boundary, boundary, num_x)
+    plt.plot(x, np.array([log_p(data, np.array([_])) for _ in x]))
+    plt.plot(x, cgmd.log_pdf(np.array([[_] for _ in x])), '--')
+    plt.show()
+
     
-    bi_num_samples = 100
-    thetae = fgmd.sample(bi_num_samples)
-    
-    x = np.random.uniform(size=[x_dim])
-    y_target = target_f(x)
-    y_hat = np.mean([f(thetae[i,:], x) for i in range(bi_num_samples)], axis=0)
-    print(y_target, y_hat)
+#    # --- `theta_hat` ---
+#    
+#    thetae = cgmd.sample(100)
+#    print(np.mean(thetae, axis=0))
+#    
+#    x = np.random.uniform(size=[x_dim])
+#    y_target = target_f(x)
+#    y_hat = np.mean([f(thetae[i,:], x) for i in range(100)], axis=0)
+#    print(y_target, y_hat)
