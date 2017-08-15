1e0ee64c1c1cd31510e2c22bfd647af51ad82f2c
pen. ciun. jian <shuiruge@hotmail.com>
2017-08-15 22:41:59 +0800

    Re-arranged; moved logs to 'nn4post_tf_experiments.py'. Double-checked gradients. Greatly different.

--- sample/nn4post_tf.py
+++ sample/nn4post_tf.py
@@ -8,204 +8,98 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.contrib.distributions import \
     Categorical, Mixture, MultivariateNormalDiag
+from tensorflow.contrib.distributions import softplus_inverse
     
     
-    
-NUM_PEAKS = 5
-DIM = 2
+NUM_PEAKS = 1
+DIM = 1
 NUM_SAMPLES = 1000
 
 
-# Set trainable variables (parameters) of CGMD
-a = tf.Variable(tf.random_uniform([NUM_PEAKS]),
-                dtype=tf.float32)
-weights = tf.nn.softmax(a)
-mu = tf.Variable(tf.random_uniform([NUM_PEAKS, DIM]),
-                 dtype=tf.float32)
-
-zeta = tf.Variable(tf.random_uniform([NUM_PEAKS, DIM]),
-                   dtype=tf.float32)
-sigma = tf.nn.softplus(zeta)
-
-# Construct CGMD as a `tf.distributions.Mixture` instance
-cat = Categorical(probs=weights)
-components = [
-    MultivariateNormalDiag(
-        loc=mu[i],
-        scale_diag=sigma[i])
-    for i in range(NUM_PEAKS)]
-cgmd = Mixture(cat=cat, components=components)
-
-
-#def elbo(log_p):
-#    """
-#    Args:
-#        log_p: Map(tf.Tensor(shape=[None, DIM], dtype=tf.float32),
-#                   tf.Tensor(shape=[], dtype=tf.float32))
-#    Returns:
-#        tf.Tensor(shape=[], dtype=tf.float32)
-#    
-#    XXX:
-#        Or returns`tf.Op`???
-#    """
-#    thetae = cgmd.sample(NUM_SAMPLES)
-#    return tf.reduce_mean(cgmd.log_prob(thetae) - log_p(thetae))
-#
-#
-#
-## -- Test
-#def log_p(thetae):
-#    return (-0.5 * tf.reduce_mean(tf.square(thetae), axis=1)
-#            -0.5 * tf.log(2 * np.pi) * DIM)
-#
-#sess = tf.Session()
-#init = tf.global_variables_initializer()
-#sess.run(init)
-#
-##with sess:
-##    
-##    with Timer():
-##        elbo_val = sess.run(elbo(log_p))
-#            
-## So far so good.
-#
-#
-#elbo_op = elbo(log_p)
-#grads_0 = tf.gradients(elbo_op, [a, mu, zeta])
-#grads = [grads_0[i] + elbo_op for i in range(3)]
-#
-#
-#with sess:
-#    
-#    elbo_val = sess.run(elbo_op)
-#    grads_0_val = sess.run(grads_0)
-#    grads_val = sess.run(grads)
-#    
-#    print(grads_val[0] - grads_0_val[0] - elbo_val)
-#    # NOT correct.
+graph = tf.Graph()
     
-
-# -- Try another way
-thetae = cgmd.sample(NUM_SAMPLES)
 def log_p(thetae):  # -- Test
-    return (-0.5 * tf.reduce_mean(tf.square(thetae-100), axis=1)
+    return (-0.5 * tf.reduce_sum(tf.square(thetae-100), axis=1)
             -0.5 * tf.log(2 * np.pi) * DIM)
-elbo = tf.reduce_mean(cgmd.log_prob(thetae) - log_p(thetae))
-grads_0 = tf.gradients(elbo, [a, mu, zeta])
-grads = [grads_0[i] + elbo for i in range(3)]
 
+with graph.as_default():
 
-
-
-''' NOTE:
+    with tf.name_scope('trainable_variables'):
+        a = tf.Variable(tf.ones([NUM_PEAKS]),
+                        dtype=tf.float32,
+                        name='a')
+        mu = tf.Variable(tf.zeros([NUM_PEAKS, DIM]),
+                         dtype=tf.float32,
+                         name='mu')
+        zeta = tf.Variable(softplus_inverse(
+                               tf.ones([NUM_PEAKS, DIM])),
+                           dtype=tf.float32,
+                           name='zeta')
     
-    As shown in ..:code:
+    with tf.name_scope('CGMD_model'):
+        with tf.name_scope('model_parameters'):
+            weights = tf.nn.softmax(a, name='weights')
+            sigma = tf.nn.softplus(zeta, name='sigma')
+        with tf.name_scope('categorical'):
+            cat = Categorical(probs=weights)
+        with tf.name_scope('Gaussian'):
+            components = [
+                MultivariateNormalDiag(
+                    loc=mu[i],
+                    scale_diag=sigma[i])
+                for i in range(NUM_PEAKS)]
+        with tf.name_scope('mixture'):
+            cgmd = Mixture(cat=cat, components=components, name='CGMD')
         
-        with tf.Session() as sess:
+    with tf.name_scope('sampling'):
+        thetae = cgmd.sample(NUM_SAMPLES, name='thetae')
             
-            sess.run(initializer)
-        
-            elbo_val, grads_0_val, grads_val = sess.run([elbo, grads_0, grads])
-        
-            for i in range(3):
-                
-                # Shall print zeros
-                print(grads_val[i] - grads_0_val[i] - elbo_val)
-        
-        >>> [ 0.  0.  0.  0.  0.]
-            [[ 0.  0.]
-             [ 0.  0.]
-             [ 0.  0.]
-             [ 0.  0.]
-             [ 0.  0.]]
-            [[  0.00000000e+00   0.00000000e+00]
-             [  0.00000000e+00   0.00000000e+00]
-             [  0.00000000e+00  -5.96046448e-08]
-             [  0.00000000e+00   0.00000000e+00]
-             [  0.00000000e+00   0.00000000e+00]]
-            
-    `elbo`, `grads_0` and `grads` shall be run in one `sess.run()`, rather than
-    ..:code:
-        
-        elbo_val = sess.run(elbo)
-        grads_0_val = sess.run(grads_0)
-        grads_val = sess.run(grads)
-        
-        for i in range(3):
-            print(grads_val[i] - grads_0_val[i] - elbo_val)
-            # shall print zeros, but NOT so.
+    with tf.name_scope('ELBO'):
+        mc_integrand = cgmd.log_prob(thetae) - log_p(thetae)
+        elbo = tf.reduce_mean(mc_integrand, name='ELBO')
+
+    with tf.name_scope('optimize'):
+        optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
+        optimize = optimizer.minimize(elbo)
     
-    The reason is that any time in one `sess.run()` calling any quantity that
-    depends one `thetae`, the `cgmd` will do sampling once. So, to keep `thetae`
-    overall in one epoch, all that depends on `thetae` shall be called in one
-    `sess.run()`.
-'''
-
-
-# --- WITHOUT Modification of Gradients ---
-
-# Un-comment this block if test WITHOUT modification of gradients
-
-optimizer = tf.train.AdamOptimizer(learning_rate=0.01)
-optimize = optimizer.minimize(elbo)
+    with tf.name_scope("summary"):
+        tf.summary.scalar('ELBO', elbo)
+        tf.summary.histogram('histogram_ELBO', elbo)
+        summary = tf.summary.merge_all()
 
 # -- Test
-with tf.Session() as sess:
+with tf.Session(graph=graph) as sess:
     
+    writer = tf.summary.FileWriter('../dat/graphs', graph)
     sess.run(tf.global_variables_initializer())
     
-    with Timer():
-        
-        for i in range(15000):
-       
-           elbo_val, _ = sess.run([elbo, optimize])
-
-           if i % 100 == 0:
-               print('step: {0}'.format(i))
-               print('elbo: {0}'.format(elbo_val))
-               print('theta sample: {0}'.format(sess.run(thetae)[0]))
-               print('-----------------------\n')
-
-
-
-## --- WITH Modification of Gradients ---
-#
-## Un-comment this block if test WITH modification of gradients
-#
-#optimizer = tf.train.AdamOptimizer(learning_rate=0.01)
-#direct_grads_to_vars = optimizer.compute_gradients(elbo)
-#real_grads_to_vars = [
-#    (grad + elbo, var)
-#    if grad is not None else (None, var)
-#    for grad, var in direct_grads_to_vars]
-#optimize = optimizer.apply_gradients(real_grads_to_vars)
-#
-## -- Test
-#with tf.Session() as sess:
-#    
-#    sess.run(tf.global_variables_initializer())
-#    
 #    with Timer():
 #        
-#        for i in range(50000):
+#        for step in range(6000):
 #       
-#           elbo_val, _ = sess.run([elbo, optimize])
-#           
-#           if i % 100 == 0:
-#               print('step: {0}'.format(i))
+#           elbo_val, _, summary_val = sess.run([elbo, optimize, summary])
+#           writer.add_summary(summary_val, global_step=step)
+#
+#           if step % 100 == 0:
+#               print('step: {0}'.format(step))
 #               print('elbo: {0}'.format(elbo_val))
 #               print('theta sample: {0}'.format(sess.run(thetae)[0]))
 #               print('-----------------------\n')
-               
-               
-''' Conclusion:
+#               
+#    weights_val, mu_val, sigma_val = sess.run([weights, mu, sigma])
+#    print('weights: {0}'.format(weights_val))
+#    print('mu: {0}'.format(mu_val))
+#    print('sigma: {0}'.format(sigma_val))
     
-    Without the modification of gradients in `optimizer` for ELBO, the
-    optimization works surprsingly great (implied by ELBO as well as by the
-    sample of `theta`). However, also surprisingly, when with the modification,
-    the optimization blow up quickly.
+    # -- Double-check gradients with `nn4post_np.py`
+    gvs = optimizer.compute_gradients(elbo)
+    print([v for g, v in gvs if g is not None])
+    grads_by_optimizer = [
+        (g.eval().tolist(), v)
+        for g, v in gvs if g is not None]
     
-    Can it be possible that TensorFlow has handed the modification for ELBO???
-    
-    TODO: To answer this, we need a double-check by numpy (i.e. `nn4post_np`).
-'''
+    for i in range(3):
+        grad = np.mean(
+            [grads_by_optimizer[i][0][0] for _ in range(1000)],
+            axis=0)
+        print(grad)
